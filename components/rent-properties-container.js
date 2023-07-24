87976746e75e5baf38ee51666007b6c58d4b1d97
Stella_Wang <stella6151@gmail.com>
2023-07-24 16:54:10 +0100

    style update

--- components/rent-properties-container.js
+++ components/rent-properties-container.js
@@ -2,39 +2,39 @@ import PropertyCard from "./propertyCard";
 
 const RentPropertiesContainer = () => {
   return (
-    <div className="w-[1920px] flex flex-col py-[86px] px-0 box-border items-center justify-start gap-[39px] text-center text-21xl text-primary-800 font-body-regular-600">
-      <div className="self-stretch flex flex-col items-center justify-start gap-[40px] max-w-[95%px] md:pl-[60px] md:pr-[60px] md:box-border">
-        <div className="w-[688px] flex flex-col items-center justify-start gap-[24px] max-w-[95%px] lg:max-w-[95%] md:self-stretch md:w-auto">
-          <div className="self-stretch relative leading-[48px] font-semibold">
+    <div className="flex flex-col py-[86px] px-0 box-border items-center justify-start gap-[39px] text-center text-21xl text-primary-800 font-body-regular-600">
+      <div className="flex flex-col items-center justify-start gap-[40px]  md:box-border">
+        <div className=" flex flex-col flex-wrap items-center justify-start gap-[24px]    md:w-auto">
+          <div className=" leading-[48px] font-semibold">
             Latest Properties of Rent
           </div>
-          <div className="self-stretch relative text-xl leading-[28px] text-lightslategray">
+          <div className=" text-xl leading-[28px] text-lightslategray">
             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
             posuere cubilia curae; Proin sodales ultrices nulla blandit
             volutpat.
           </div>
         </div>
-        <div className="self-stretch flex flex-row flex-wrap py-0 px-2.5 items-center justify-center gap-[32px]">
+        <div className="flex flex-row flex-wrap py-0 px-2.5 items-center justify-center gap-[32px]">
           <PropertyCard
           imgUrl="/unsplashrlwe8f8anoc7@2x.png"
-          name=''
-          price='12200'
+          name='92 ALLIUM PLACE, ORLANDO FL 32827'
+          price={22200}
             
           />
           <PropertyCard
-           imgUrl="/unsplashrlwe8f8anoc7@2x.png"
-           name=''
-           price='12200'
+           imgUrl="/unsplashrlwe8f8anoc8@2x.png"
+           name='92 ALLIUM PLACE, ORLANDO FL 32827'
+           price={18200}
           />
           <PropertyCard
-            imgUrl="/unsplashrlwe8f8anoc7@2x.png"
-            name=''
-            price='12200'
+            imgUrl="/unsplashrlwe8f8anoc9@2x.png"
+            name='92 ALLIUM PLACE, ORLANDO FL 32827'
+            price={16200}
           />
           <PropertyCard
-            imgUrl="/unsplashrlwe8f8anoc7@2x.png"
-            name=''
-            price='12200'
+            imgUrl="/unsplashrlwe8f8anoc10@2x.png"
+            name='92 ALLIUM PLACE, ORLANDO FL 32827'
+            price={12200}
           />
         </div>
       </div>
